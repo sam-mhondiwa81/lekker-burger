bc62d7c1f6c550bc739fed274088bb33d524d4d4
samalo <smhonds@gmail.com>
2019-06-12 11:57:07 +0200

    Comming app file

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          Coding is just lekker
         </p>
         <a
           className="App-link"
